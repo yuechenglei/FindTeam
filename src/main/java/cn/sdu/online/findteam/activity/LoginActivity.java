e23e0ba10929b2466d61c779ef1dd910112834de
yuechenglei <1711723939@qq.com>
2015-07-15 14:27:13 +0800

    。。

---
+++ src/main/java/cn/sdu/online/findteam/activity/LoginActivity.java
@@ -0,0 +1,18 @@
+package cn.sdu.online.findteam.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import cn.sdu.online.findteam.R;
+
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+}
---
+++ src/main/java/cn/sdu/online/findteam/share/MyApplication.java
@@ -0,0 +1,9 @@
+package cn.sdu.online.findteam.share;
+
+import android.app.Application;
+
+/**
+ * Created by chenglei on 2015/7/15.
+ */
+public class MyApplication extends Application{
+}
