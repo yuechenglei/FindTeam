5c721fbe00f98fd9b4968af85e3c8e9ec2b91fff
846425567 <846425567@qq.com>
2015-07-19 23:55:52 +0800

    修复了ActionBar的布局，写了ActionBar上搜索按钮的监听

--- src/main/java/cn/sdu/online/findteam/activity/MainActivity.java
+++ src/main/java/cn/sdu/online/findteam/activity/MainActivity.java
@@ -3,26 +3,24 @@ package cn.sdu.online.findteam.activity;
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.app.Activity;
-import android.content.Intent;
+import android.content.Context;
 import android.content.res.Configuration;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewConfiguration;
 import android.view.Window;
+import android.view.animation.Animation;
+import android.widget.ActionMenuView;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.SearchView;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
-import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
@@ -40,17 +38,29 @@ public class MainActivity extends Activity implements XListView.IXListViewListen
     private ListViewAdapter adapter;
 
     private DrawerLayout mDrawerLayout;
-    private ListView mDrawerList;
+    private RelativeLayout mDrawerRelative;
     private ActionBarDrawerToggle mDrawerToggle;
     private DrawerArrowDrawable drawerArrow;
-    private boolean drawerArrowColor;
+
+    /**
+     * 记录actionsearch按钮的点击状态，true为不搜索状态，false为搜索栏弹出状态
+     */
+    private boolean acState;
+    /**
+     * 主界面搜索按钮
+     */
+    private Button actionsearch;
+    private Button searchButton;
+    /**
+     * 主界面的搜索框布局
+     */
+    private LinearLayout searchLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_ACTION_BAR);
         setContentView(R.layout.activity_main);
-
         listView = (XListView) findViewById(R.id.listview);
         listView.setXListViewListener(this);
         listView.setPullLoadEnable(true);
@@ -72,24 +82,31 @@ public class MainActivity extends Activity implements XListView.IXListViewListen
             @Override
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
-         // TODO Auto-generated method stub
-         Log.v("listview", "我被duanan点击");
-         }
-         });
+                // TODO Auto-generated method stub
+                Log.v("listview", "我被duanan点击");
+            }
+        });
 
-         /**
-         * 侧边栏的初始化
+        searchButton = (Button) findViewById(R.id.search_button);
+        searchLayout = (LinearLayout) findViewById(R.id.search_layout);
+        /**
+         *设置ActionBar的自定义布局。
          */
-        ActionBar ab = this.getActionBar();
+        setActionBarLayout(R.layout.actionbar_layout);
+        /**
+         * 定义ActionBar上的搜索按钮,并设置监听
+         */
+        actionsearch = (Button) findViewById(R.id.action_search);
+        actionsearch.setOnClickListener(new ActionSearchListener());
 
         /**
-         *左上角设置一个按钮，并设置可点击
+         * 初始化 acState 为true
          */
-        ab.setDisplayHomeAsUpEnabled(true);
-        ab.setHomeButtonEnabled(true);
+        acState = true;
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerList = (ListView) findViewById(R.id.navdrawer);
+        // mDrawerList = (ListView) findViewById(R.id.navdrawer);
+        mDrawerRelative=(RelativeLayout)findViewById(R.id.navdrawer);
 
 
         drawerArrow = new DrawerArrowDrawable(this) {
@@ -113,93 +130,100 @@ public class MainActivity extends Activity implements XListView.IXListViewListen
         };
         mDrawerLayout.setDrawerListener(mDrawerToggle);
         mDrawerToggle.syncState();
-
-
-        String[] values = new String[]{
-                "Stop Animation (Back icon)",
-                "Stop Animation (Home icon)",
-                "Start Animation",
-                "Change Color",
-                "GitHub Page",
-                "Share",
-                "Rate"
-        };
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, values);
-        mDrawerList.setAdapter(adapter);
-        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-                switch (position) {
-                    case 0:
-                        mDrawerToggle.setAnimateEnabled(false);
-                        drawerArrow.setProgress(1f);
-                        break;
-                    case 1:
-                        mDrawerToggle.setAnimateEnabled(false);
-                        drawerArrow.setProgress(0f);
-                        break;
-                    case 2:
-                        mDrawerToggle.setAnimateEnabled(true);
-                        mDrawerToggle.syncState();
-                        break;
-                    case 3:
-                        if (drawerArrowColor) {
-                            drawerArrowColor = false;
-                            drawerArrow.setColor(R.color.ldrawer_color);
-                        } else {
-                            drawerArrowColor = true;
-                            drawerArrow.setColor(R.color.drawer_arrow_second_color);
-                        }
-                        mDrawerToggle.syncState();
-                        break;
-                    case 4:
-                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/IkiMuhendis/LDrawer"));
-                        startActivity(browserIntent);
-                        break;
-                    case 5:
-                        Intent share = new Intent(Intent.ACTION_SEND);
-                        share.setType("text/plain");
-                        share.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                        share.putExtra(Intent.EXTRA_SUBJECT,
-                                getString(R.string.app_name));
-                        share.putExtra(Intent.EXTRA_TEXT, getString(R.string.app_description) + "\n" +
-                                "GitHub Page :  https://github.com/IkiMuhendis/LDrawer\n" +
-                                "Sample App : https://play.google.com/store/apps/details?id=" +
-                                getPackageName());
-                        startActivity(Intent.createChooser(share,
-                                getString(R.string.app_name)));
-                        break;
-                    case 6:
-                        String appUrl = "https://play.google.com/store/apps/details?id=" + getPackageName();
-                        Intent rateIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(appUrl));
-                        startActivity(rateIntent);
-                        break;
-                }
-
-            }
-        });
     }
 
-    @Override
+    /** @param layoutId 布局Id
+    *
+            * */
+    public void setActionBarLayout( int layoutId ){
+        ActionBar actionBar = getActionBar();
+        if( null != actionBar ){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+            actionBar.setDisplayShowCustomEnabled(true);
+            LayoutInflater inflator = (LayoutInflater)this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View v = inflator.inflate(layoutId, null);
+            ActionBar.LayoutParams layout = new ActionBar.LayoutParams(ActionMenuView.LayoutParams.FILL_PARENT, ActionMenuView.LayoutParams.FILL_PARENT);
+            actionBar.setCustomView(v,layout);
+        }
+    }
+
+
+/*    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        startSearchbt = MenuItemCompat.getActionView(searchItem);
+        startSearchbt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchLayout.setVisibility(View.VISIBLE);
+                xlistTranslate = AnimationUtils.loadAnimation(MainActivity.this,
+                        R.anim.xlistviewtranslate);
+                listView.startAnimation(xlistTranslate);
+            }
+        });
         return super.onCreateOptionsMenu(menu);
+    }*/
+
+//        MenuItem searchItem = menu.findItem(R.id.ab_search);
+//        SearchView searchView = (SearchView) MenuItemCompat
+//                .getActionView(searchItem);
+//        if(searchView==null)
+//            return true;
+//        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+//            @Override
+//            public boolean onQueryTextChange(String arg0) {
+//                // TODO Auto-generated method stub
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean onQueryTextSubmit(String arg0) {
+//                // TODO Auto-generated method stub
+//                return false;
+//            }
+//
+//        });
+//        return true;
+
+
+    private class ActionSearchListener implements View.OnClickListener{
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        @Override
+        public void onClick(View v) {
+            if(acState == true) {
+                searchLayout.setVisibility(View.VISIBLE);
+                params.setMargins(0, 110, 0, 0);
+                listView.setLayoutParams(params);
+                acState = false;
+            }
+            else {
+                searchLayout.setVisibility(View.GONE);
+                params.setMargins(0,0,0,0);
+                listView.setLayoutParams(params);
+                acState = true;
+            }
+        }
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == android.R.id.home) {
-            if (mDrawerLayout.isDrawerOpen(mDrawerList)) {
-                mDrawerLayout.closeDrawer(mDrawerList);
+            if (mDrawerLayout.isDrawerOpen(mDrawerRelative)) {
+                mDrawerLayout.closeDrawer(mDrawerRelative);
             } else {
-                mDrawerLayout.openDrawer(mDrawerList);
+                mDrawerLayout.openDrawer(mDrawerRelative);
             }
         }
         return super.onOptionsItemSelected(item);
     }
+
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
@@ -216,6 +240,7 @@ public class MainActivity extends Activity implements XListView.IXListViewListen
 
     /**
      * 下拉刷新实现
+     *
      * @return
      */
     private List<HashMap<String, String>> getListDate() {
@@ -311,6 +336,8 @@ public class MainActivity extends Activity implements XListView.IXListViewListen
 
         return sbBuffer.toString();
     }
+
+
 }
 
 
